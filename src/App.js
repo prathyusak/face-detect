ef0e67fee7dee32a1039a9a00cea1fcd5e1f3010
prathyusak <kandaprathyusha@gmail.com>
2020-10-11 19:25:33 -0700

    make it better

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ loadUser = (data) => {
   }
 
 calculateFaceLocation = (data) => {
-  console.log(data)
+  console.log(data.rawData.outputs[0].data.regions[0].data.concepts)
   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
   const image = document.getElementById('inputimage');
   const width = Number(image.width);

0739b1f525e85c4aa5f77c4166ff0238225f1b55
prathyusak <kandaprathyusha@gmail.com>
2020-10-11 20:31:04 -0700

    make it better

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -8,14 +8,14 @@ function FaceRecognition({imageUrl, box, values}) {
   	  <div className='absolute mt2'>
   	    <img id='inputimage' alt='' src={imageUrl} width='300px' height='auto'/>
   	    <div className='bounding-box' style={{top: box.topRow, right: box.rightCol ,bottom: box.bottomRow, left: box.leftCol}}></div>
-  	    <div> Demographic information 
+  	    </div>
+  	   <div className='center ma'> Demographic information 
   	    <ul>
   	    {values.map(item => {
   	    	return <li>{item.name}</li>;
   	    })}
   	    </ul>
   	    </div>
-  	  </div>
    
     </div>
   )
